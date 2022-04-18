36a8186fc65b323aee5ca80968287613aa3e5f45
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-18 16:30:18 +0530

    refactored: analyseMood method can just return SAD to pass this Test Case

---
+++ src/test/java/MoodAnalyzerTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyzerTest {
+    @Test
+    void givenMood_AnalyseSadMood_ResultShouldMatch() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("I am in sad mood");
+        Assertions.assertEquals("SAD",mood);
+    }
+
+    @Test
+    void givenMood_AnalyseHappyMood_ResultShouldMatch() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+        String mood = moodAnalyzer.analyzeMood("I am in Happy mood");
+        Assertions.assertEquals("HAPPY",mood);
+    }
+}
