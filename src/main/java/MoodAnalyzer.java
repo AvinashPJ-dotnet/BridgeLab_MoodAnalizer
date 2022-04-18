4da335d84f5f89e69b984f2284a4e06b33eedb92
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-18 16:24:32 +0530

    Added: analyze mood function

---
+++ src/main/java/MoodAnalyzer.java
@@ -0,0 +1,8 @@
+public class MoodAnalyzer {
+
+    public String analyzeMood(String message) {
+        if(message.toLowerCase().contains("sad"))
+            return "SAD";
+        return "HAPPY";
+    }
+}
